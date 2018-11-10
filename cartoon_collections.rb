5b986e503929089cb90461d836fcd42ff78303ec
Stephen Argentina <stephena9871@gmail.com>
2018-11-10 01:03:52 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  count = 1
+  for x in array
+    puts "#{count}. #{x}"
+    count += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_list = []
+  for x in array
+    new_list.push("#{x.upcase}!")
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+    array.any? |i|
+      i > 4
+    end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  answer = array.find(cheese_types)
+  return answer
 end
